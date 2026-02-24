e3a63ce7e320f14974bc24db39cffee1951060b7
emil-nabil333 <elbrinselomda@yahoo.com>
2026-02-24 19:23:57 +0200

    Add files via upload

---
+++ backup-openatv_7.6.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+##setup command=wget https://raw.githubusercontent.com/emilnabil/backup-images/refs/heads/main/backup-openatv_7.6.sh -O - | /bin/sh
+##################################
+wget https://raw.githubusercontent.com/emil237/updates-enigma/main/update-all-python.sh -O - | /bin/sh
+echo "installed depends"
+
+cd /tmp
+echo "install backup openatv_7.6"
+
+
+
+
