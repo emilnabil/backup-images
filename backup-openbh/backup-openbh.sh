4d26437fe53610a8b5b5e9a88eb5855cbf43d6bf
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-02 11:39:04 +0200

    Add files via upload

--- backup-openbh/backup-openbh.sh
+++ backup-openbh/backup-openbh.sh
@@ -146,11 +146,12 @@ urls=(
     "https://dreambox4u.com/emilnabil237/KeyAdder/installer.sh"
     "https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/MultiCamAdder/installer.sh"
     "https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/MultiIptvAdder/installer.sh"
-    "https://raw.githubusercontent.com/emilnabil/multi-stalkerpro/main/installer.sh"
     "https://dreambox4u.com/emilnabil237/plugins/NewVirtualKeyBoard/installer.sh"
     "https://dreambox4u.com/emilnabil237/plugins/RaedQuickSignal/installer.sh"
     "https://raw.githubusercontent.com/emilnabil/download-plugins/refs/heads/main/SmartAddonspanel/smart-Panel.sh"
     "https://dreambox4u.com/emilnabil237/plugins/xtreamity/installer.sh"
+
+"https://raw.githubusercontent.com/emilnabil/channel-emil-nabil/main/installer.sh"
 
 "https://raw.githubusercontent.com/popking159/skins/refs/heads/main/aglarepli/installer.sh"
     "https://dreambox4u.com/emilnabil237/emu/installer-cccam.sh"
@@ -233,10 +234,5 @@ fi
 echo "Script finished at: $(date)" >&3
 
 exit 0
-
-
-
-
-
 
 
