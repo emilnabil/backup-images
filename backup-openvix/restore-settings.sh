094d0a81d6bf5e4df53f64427ced82e6c30c0be9
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-14 10:27:25 +0200

    Add files via upload

---
+++ backup-openvix/restore-settings.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+## setup command=wget https://github.com/emilnabil/backup-images/raw/refs/heads/main/backup-openpli/restore-settings.sh -O - | /bin/sh
+##################################
+cd /tmp || exit 1
+
+if wget -q "https://github.com/emilnabil/backup-images/raw/refs/heads/main/backup-openpli/settings_backup_openPli.tar.gz"; then
+    echo "Download completed successfully"
+else
+    echo "Download failed"
+    exit 1
+fi
+
+sleep 2
+
+if [ -f "settings_backup_openPli.tar.gz" ]; then
+    tar -xzf settings_backup_openPli.tar.gz -C /
+    sleep 3
+    rm -f settings_backup_openPli.tar.gz
+else
+    echo "Backup file not found"
+    exit 1
+fi
+
+sleep 5
+rm -rf /var/cache/opkg/* 2>/dev/null
+rm -rf /var/lib/opkg/lists/* 2>/dev/null
+rm -f /run/opkg.lock 2>/dev/null
+
+sleep 5
+echo "Process completed successfully"
+sleep 5
+exit 0
