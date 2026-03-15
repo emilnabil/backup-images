e09d42bfb497c2379ae9e8767553debb476d6794
emil-nabil333 <elbrinselomda@yahoo.com>
2026-03-15 14:28:49 +0200

    Add files via upload

--- Backup-OpenSpa/backup-openspa.sh
+++ Backup-OpenSpa/backup-openspa.sh
@@ -152,13 +152,13 @@ urls=(
 "https://raw.githubusercontent.com/popking159/skins/refs/heads/main/aglareatv/installer.sh"
 
 "https://raw.githubusercontent.com/islam-2412/IPKS/refs/heads/main/fury/installer.sh"
-
-"https://raw.githubusercontent.com/emilnabil/channel-emil-nabil/main/installer.sh"
     "https://dreambox4u.com/emilnabil237/emu/installer-cccam.sh"
     "https://dreambox4u.com/emilnabil237/emu/installer-ncam.sh"
     "https://raw.githubusercontent.com/levi-45/Levi45Emulator/main/installer.sh"
 
 "https://github.com/emilnabil/backup-images/raw/refs/heads/main/Backup-OpenSpa/restore-settings.sh"
+
+"https://raw.githubusercontent.com/emilnabil/channel-emil-nabil/main/installer.sh"
 )
 
 set +e
@@ -211,6 +211,7 @@ fi
 echo "Script finished at: $(date)" >&3
 
 exit 0
+
 
 
 
