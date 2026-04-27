be2471eef387b77fc99f76113c20bc7d8b7af8d2
emil-nabil333 <elbrinselomda@yahoo.com>
2026-04-27 14:30:00 +0300

    Add files via upload

--- Backup-openpli-py2/backup-openpli-py2.sh
+++ Backup-openpli-py2/backup-openpli-py2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## setup command=wget https://github.com/emilnabil/backup-images/raw/refs/heads/main/backup-openpli/backup-openpli.sh -O - | /bin/sh
+## setup command=wget https://github.com/emilnabil/backup-images/raw/refs/heads/main/Backup-openpli-py2/backup-openpli-py2.sh -O - | /bin/sh
 ##################################
 
 reboot() {
@@ -190,4 +190,5 @@ reboot -f
 init 6
 
 exit 0
+
 
